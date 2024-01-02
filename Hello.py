d00cd2207d47225d246212ac05e3418d04674c8d
Adriel Silva Camargos <34457423+adrielscamargos@users.noreply.github.com>
2024-01-02 13:47:09 -0300

    First commit by Adriel
    Curso Cod3r.com da udemy 
    Instrutor :Leonardo Moura Leitao, Arquiteto de Software.

---
+++ Hello.py
@@ -0,0 +1,7 @@
+print ("Hi, My name is Adriel and i work now with Python")
+print("Hello, World!")
+a = 7
+b = 3 
+
+a, b = b, a
+print(a, b)
---
+++ fundamentos_projetos/area_circulo_v1.py
@@ -0,0 +1,5 @@
+#"""Projeto de area da circurferencia com python do curso da Cod3r.com na Udemy
+#aluno: Adriel Silva Camargos"""
+pi = 3.1415
+raio = 15
+print('area_circuferencia', pi * raio **2)
